856f58fa1b77d049f785643fd9317a561ba03a97
1127071993@qq.com <aini2009626>
2023-11-20 21:01:44 +0800

    创建热门推荐搜索目录，嵌入输入框，添加控制状态变量 调整远程输入搜索内容样式布局 处理热门推荐与搜索内容列表显示冲突问题

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,17 +1,53 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import {
   UnorderedListOutlined,
   SearchOutlined,
-} from '@ant-design/icons';
-import { Input } from 'antd';
+  PlaySquareOutlined,
+  CameraOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import { Input, AutoComplete } from "antd";
 import logo from "@/assets/logo.png";
-import "./index.scss";
+import styles from "./index.module.scss";
 
 export default function Header() {
+  // AutoComplete相关
+  const mockVal = (str, repeat = 1) => ({
+    value: str.repeat(repeat),
+  });
+  const [options, setOptions] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const getPanelValue = (searchText) => {
+    setSearchText(searchText);
+    if (!searchText) {
+      setShowHot(true);
+      return [];
+    } else {
+      setShowHot(false);
+      return [
+        mockVal(searchText),
+        mockVal(searchText, 2),
+        mockVal(searchText, 3),
+      ];
+    }
+  };
+  const blurHandle = () => {
+    setShowHot(false);
+  };
+
+  //热门推荐
+  const [showHot, setShowHot] = useState(false);
+  const hotList = ["颜值", "漫展"];
+  const handleFouce = () => {
+    if (!searchText) {
+      setShowHot(true);
+    }
+  };
+
   return (
-    <div className="header-wrap">
+    <div className={styles.headerwrap}>
       <div className="networkBarWrapper">
         <div id="js-networkBar" className="networkBar">
           <ul className="networkListContent">
@@ -80,18 +116,44 @@ export default function Header() {
           </ul>
         </div>
       </div>
-
-      <div className="header-in">
+      <div className="search-wrap">
+        <div className="header-in">
           <div className="left">
             <div className="desktopNavigation">
               <UnorderedListOutlined className="hamburgerIcon" />
             </div>
-            <img className="logo" src={logo.src} alt="logo"/>
+            <img className="logo" src={logo.src} alt="logo" />
           </div>
-          <div className="mid">
-             <Input size="large" placeholder="搜索内容" bordered={false} prefix={<SearchOutlined />} />
+          <div className="mid-wrap">
+            <div className="mid">
+              <SearchOutlined className="search" />
+              <div className="input-wrap">
+                <AutoComplete
+                  style={{ width: "100%", height: "40px" }}
+                  allowClear
+                  options={options}
+                  placeholder="搜索内容"
+                  onSearch={(text) => setOptions(getPanelValue(text))}
+                  onSelect={globalThis.console.log}
+                  bordered={false}
+                  onFocus={handleFouce}
+                  onBlur={blurHandle}
+                  popupClassName="search-results"
+                />
+                <div className={showHot ? "hot show" : "hot"}>
+                  <div className="title">热门搜索</div>
+                  <div className="item">漫展</div>
+                </div>
+              </div>
+            </div>
+            <PlaySquareOutlined className="play-square" />
+            <CameraOutlined className="camera" />
           </div>
-          <div className="right"></div>
+
+          <div className="right">
+            <UserOutlined className="user" />
+          </div>
+        </div>
       </div>
     </div>
   );
